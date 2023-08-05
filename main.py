1e7c50c978c78185fb1ab0baf5f695febec66b6d
shuvopaul601 <67578747+shuvopaul601@users.noreply.github.com>
2023-08-05 18:05:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,45 @@
-#!/user/bin/python
+#!/usr/bin/python
 
 import socket
 from termcolor import colored
+import concurrent.futures
 
-sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-socket.setdefaulttimeout(1)
+def get_host_info(target):
+    try:
+        ip_address = socket.gethostbyname(target)
+        return ip_address
+    except socket.gaierror:
+        return "Unknown IP"
 
-host = input("[*] Enter The Host To Scan: ")
+def get_service_name(port):
+    try:
+        service_name = socket.getservbyport(port)
+        return service_name
+    except:
+        return "Unknown"
 
-def portscanner(port):
-    if sock.connect_ex((host,port)):
-        print(colored("Port %d is closed" % (port), 'red'))
-    else:
-        print(colored("Port %d is open" % (port), 'green'))
+def portscanner(target, port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(1)
+    result = sock.connect_ex((target, port))
+    sock.close()
 
-for port in range(1,1000):
-    portscanner(port)
+    if result == 0:
+        service_name = get_service_name(port)
+        print(colored(f"Port {port} is open - Service: {service_name}", 'green'))
+
+def main():
+    target = input("[*] Enter the Domain Name or IP Address to Scan: ")
+    ip_address = get_host_info(target)
+
+    print(f"Scanning ports for {target} ({ip_address})\n")
+
+    # Increase the number of workers based on your system's capabilities
+    max_workers = 50
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
+        port_range = range(1, 1000)
+        executor.map(lambda port: portscanner(ip_address, port), port_range)
+
+if __name__ == "__main__":
+    main()
